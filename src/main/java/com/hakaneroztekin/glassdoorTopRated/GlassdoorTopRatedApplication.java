4b548ef20320b43abe60a538f5dd024270c6cbfb
eroztekin <eroztekin@itu.edu.tr>
2019-09-25 05:33:19 +0300

    initialize Spring project with Spring Initializr

---
+++ src/main/java/com/hakaneroztekin/glassdoorTopRated/GlassdoorTopRatedApplication.java
@@ -0,0 +1,13 @@
+package com.hakaneroztekin.glassdoorTopRated;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlassdoorTopRatedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlassdoorTopRatedApplication.class, args);
+	}
+
+}
