12957ec8a1df8aa801d953b194646864f3190e90
eroztekin <eroztekin@itu.edu.tr>
2019-09-26 06:29:07 +0300

    update schema commands used by Docker PostgreSQL container

--- src/main/java/schema.sql
+++ src/main/java/schema.sql
@@ -1,1 +1,26 @@
-CREATE DATABASE IF NOT EXISTS glassdoor_db;
+CREATE DATABASE IF NOT EXISTS scraperdb;
+
+CREATE TABLE IF NOT EXISTS company (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(20),
+    profile_url VARCHAR(40),
+    picture_url VARCHAR(80),
+    rate NUMERIC(2, 1),
+    total_review INT
+);
+
+# Example dummy insert
+INSERT INTO company(
+    name, 
+    profile_url, 
+    picture_url, 
+    rate, 
+    total_review
+) 
+VALUES  (
+    'companyName',
+    'companyProfileURL',
+    'companyPictureURL',
+    4.5,
+    20
+);
