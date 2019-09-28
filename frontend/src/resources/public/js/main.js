cdab70826b8479ef87463439f659f25edc6e73c5
eroztekin <eroztekin@itu.edu.tr>
2019-09-28 10:58:55 +0300

    connect to Glassdoor

--- frontend/src/resources/public/js/main.js
+++ frontend/src/resources/public/js/main.js
@@ -1,5 +1,23 @@
-const request = require('request');
+const requestPromise = require('request-promise');
+const cheerio = require('cheerio');
 
-request('http://stackabuse.com', function(err, res, body) {
-    console.log(body);
-});
+connect();
+
+function connect() {
+    console.log("Connecting the website");
+    // for version 1 of the app, Istanbul is used as the city
+    let URL = "https://www.glassdoor.com/Reviews/istanbul-reviews-SRCH_IL.0,8_IM1160.htm";
+    requestPromise({
+        url: URL,
+        headers: {
+            'User-Agent': 'Request-Promise'
+        }
+    })
+        .then(function(response) {
+            console.log(response);
+        })
+        .catch(function(err){
+            console.log("error on connection");
+            console.log(err);
+        });
+}
