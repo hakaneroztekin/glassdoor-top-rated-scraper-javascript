7c3dbf78978886b6148c4eeb3459ca6766b52e3e
eroztekin <eroztekin@itu.edu.tr>
2019-09-29 14:02:24 +0300

    hello world! :)

--- frontend/react-app/src/index.js
+++ frontend/react-app/src/index.js
@@ -4,7 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <h1>Hello world!</h1>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
