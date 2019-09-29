d81034d2ddce66ae086354889374f3586afba416
eroztekin <eroztekin@itu.edu.tr>
2019-09-29 15:24:01 +0300

    add title

--- frontend/react-app/src/index.js
+++ frontend/react-app/src/index.js
@@ -4,25 +4,33 @@ import './index.css';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-// 'Google',
-//     'https://www.glassdoor.com/Overview/Working-at-Google-EI_IE9079.11,17.htm',
-//     'https://media.glassdoor.com/sql/9079/google-squarelogo-1441130773284.png',
-//     4.4,
-//     12000
+function App() {
+    return (
+        <div>
+            <div>
+                <Header />
+            </div>
+            <div>
+                <Company />
+            </div>
+        </div>
+    );
+}
+
+function Header(props) {
+    return (
+            <head>
+                <title>Top Rated Companies @ Glassdoor</title>
+            </head>
+    );
+}
+
 function Company(props) {
     return (
         <div className="Company">
             <h1>Google</h1>
             <h2>4.4 Point</h2>
             <h2>12000 Reviews</h2>
-        </div>
-    )
-}
-
-function App() {
-    return (
-        <div>
-            <Company />
         </div>
     );
 }
