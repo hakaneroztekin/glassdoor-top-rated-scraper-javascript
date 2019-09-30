57e2ef4927050dc5ad5c4ee24b516e98399a707a
eroztekin <eroztekin@itu.edu.tr>
2019-09-30 08:32:48 +0300

    tidy the code

--- frontend/react-app/src/App.js
+++ frontend/react-app/src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { ThemeProvider } from '@material-ui/styles';
+import {ThemeProvider} from '@material-ui/styles';
 import Grid from "@material-ui/core/Grid";
 import List from "@material-ui/core/List";
-import {Header} from "./Components/Company";
 import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 import {blue, indigo} from "@material-ui/core/colors";
 
@@ -12,49 +10,49 @@ import Menu from './Components/Menu.js';
 import Company from './Components/Company.js';
 
 const theme = createMuiTheme({
-  palette: {
-    secondary: {
-      main: blue[900]
+    palette: {
+        secondary: {
+            main: blue[900]
+        },
+        primary: {
+            main: indigo[700]
+        }
     },
-    primary: {
-      main: indigo[700]
+    typography: {
+        // Use the system font instead of the default Roboto font.
+        fontFamily: [
+            '"Lato"',
+            'sans-serif'
+        ].join(',')
     }
-  },
-  typography: {
-    // Use the system font instead of the default Roboto font.
-    fontFamily: [
-      '"Lato"',
-      'sans-serif'
-    ].join(',')
-  }
 });
 
 function App() {
-  // const classes = useStyles();
-  return (
-      <div>
-        <ThemeProvider theme={theme}>
-          <div>
-            <div>
-              <Menu />
-            </div>
-            <div>
-              <Grid
-                  container
-                  justify="space-evenly"
-                  alignItems="center"
-              >
-                <List>
-                  <Company />
-                  <Company />
-                  <Company />
-                </List>
-              </Grid>
-            </div>
-          </div>
-        </ThemeProvider>
-      </div>
-  );
+    // const classes = useStyles();
+    return (
+        <div>
+            <ThemeProvider theme={theme}>
+                <div>
+                    <div>
+                        <Menu/>
+                    </div>
+                    <div>
+                        <Grid
+                            container
+                            justify="space-evenly"
+                            alignItems="center"
+                        >
+                            <List>
+                                <Company/>
+                                <Company/>
+                                <Company/>
+                            </List>
+                        </Grid>
+                    </div>
+                </div>
+            </ThemeProvider>
+        </div>
+    );
 }
 
 export default App;
--- frontend/react-app/src/Components/Company.js
+++ frontend/react-app/src/Components/Company.js
@@ -1,18 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 // import './index.css';
-import App from '../App';
-import * as serviceWorker from '../serviceWorker';
 
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import Avatar from "@material-ui/core/Avatar";
-import AppBar from "@material-ui/core/AppBar";
-import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
-import Toolbar from "@material-ui/core/Toolbar";
 
 const useStyles = makeStyles(theme => ({
     listItem: {
